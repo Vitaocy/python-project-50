7ef4d0c9e58499cacf1730e2089db5d489fa5cf5
имя фамилия < >
2026-01-16 19:16:48 +0300

    Add Option to set format of output

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -2,8 +2,23 @@ import argparse
 from gendiff.gendiff import generate_diff
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        metavar='FORMAT',
+        help='set format of output'
+    )
+    
+
+    args = parser.parse_args()
+    print(args)
+    # generate_diff()
+    
+
 
 
 if __name__ == "__main__":
