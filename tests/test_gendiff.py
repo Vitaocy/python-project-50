cb84a42472a5e3b1df1b4d62965641415b568a6d
имя фамилия < >
2026-01-20 14:50:42 +0300

    feat: enhance tests with error handling for non-existing files and incorrect formats

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+import pytest
+
 from gendiff.scripts.gendiff import generate_diff
 
 
@@ -51,3 +53,21 @@ def test_generate_diff_recursive_yaml_to_json():
     second_file = 'examples/file_recursive2.yaml'
     diff = generate_diff(first_file, second_file, format_name='json')
     assert diff == read_test_data('result_rec_json.txt')
+
+
+def test_not_existing_file():
+    first_file = 'nonfile1.json'
+    second_file = 'examples/file2.json'
+    # файл не существует
+    with pytest.raises(FileNotFoundError):
+        generate_diff(first_file, second_file)
+
+
+def test_incorrect_format():
+    first_file = 'examples/file1.json'
+    second_file = 'examples/file2.json'
+    with pytest.raises(ValueError):
+        generate_diff(first_file, second_file, format_name='unknown_format')
+
+    with pytest.raises(ValueError):
+        generate_diff(first_file, second_file, format_name=1)
